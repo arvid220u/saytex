aa843dedcd771c20c971ff254479ea72b8526eb4
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-11-08 18:10:39 -0500

    add region key to config

--- app/app.py
+++ app/app.py
@@ -9,4 +9,7 @@ import config
 
 @app.route("/")
 def index():
-    return render_template('index.html', microsoft_speech_api_key = config.MICROSOFT_SPEECH_API_KEY, api_url = config.API_URL)
+    return render_template('index.html',    microsoft_speech_api_key = config.MICROSOFT_SPEECH_API_KEY, 
+                                            microsoft_region_key = config.MICROSOFT_REGION_KEY, 
+                                            api_url = config.API_URL
+                                        )
--- app/config_template.py
+++ app/config_template.py
@@ -3,5 +3,6 @@
 # `config.py` is ignored by git
 
 MICROSOFT_SPEECH_API_KEY = 'insert microsoft cognitive services speech API key here'
+MICROSOFT_REGION_KEY = 'insert microsoft region key here' # e.g. eastus
 
 API_URL = '127.0.0.1:8000' # DEVELOPMENT
