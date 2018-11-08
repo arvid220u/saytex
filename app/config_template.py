61b1fd9dcb5c27a61c4ae24782821d46757afb69
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-11-08 17:54:00 -0500

    add config template

---
+++ app/config_template.py
@@ -0,0 +1,7 @@
+# Configuration file
+# Should be copied into `config.py` where real values should be entered
+# `config.py` is ignored by git
+
+MICROSOFT_SPEECH_API_KEY = 'insert microsoft cognitive services speech API key here'
+
+API_URL = '127.0.0.1:3000' # DEVELOPMENT
