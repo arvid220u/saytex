5fa7cc6c8641327bad2856062a3e9bd085db2cdd
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-11-08 17:54:34 -0500

    load index.html template in app.py

--- app/app.py
+++ app/app.py
@@ -4,20 +4,9 @@ from flask import *
 app = Flask(__name__)
 app.config["PREFERRED_URL_SCHEME"] = "https"
 
+import config
 
 
-# IMPORTANT THINGS START HERE
-
-
-
-from main import text2latex, endtext
-@app.route("/api/<string:text>")
-def API(text):
-    return text2latex(text)
-
-@app.route("/api2/<string:text>")
-def API2(text):
-    print("api2")
-    return endtext(text)
-
-
+@app.route("/")
+def index():
+    return render_template('index.html', microsoft_speech_api_key = config.MICROSOFT_SPEECH_API_KEY, api_url = config.API_URL)
