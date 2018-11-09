1e46b4357cd765f511bef5d137db01de7a081a62
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2018-11-09 00:02:48 -0500

    remove old website code

--- website/flaskserver/gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-#daemon = True
-# 1 core should equal 2*1+1=3 workers
-workers = 3
-# we want to use threads for concurrency. we estimate that a maximum of 3*7=21 users will use the application simultaneously
-worker_class = "gthread"
-threads = 7
-#accesslog = "logs/access.log"
-#errorlog = "logs/error.log"
