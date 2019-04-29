40bf9cb720e86520ffce12395015b8058ea7605e
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2019-04-29 15:45:27 -0400

    create layer package

---
+++ saytexplus/layers/__init__.py
@@ -0,0 +1,21 @@
+"""
+Defines all layers, used for converting natural language into SayTeX Syntax.
+All layers should subclass the layer interface defined in layers.py.
+"""
+
+from layers import *
+
+"""
+The layer_id_to_class dictionary contains a mapping from a string identifying
+a layer to the class implementing the layer. The string is used in config.py.
+"""
+layer_id_to_class = {
+    "speech_recognition_error_correction": None,
+    "capitalization": None,
+    "spoken_number_recognition": None,
+    "synonym_standardization": None,
+    "from_to_recognition": None,
+    "divided_by_recognition": None,
+    "auto_completion_to_avoid_compile_errors": None,
+    "prettification": None
+}
---
+++ saytexplus/layers/layer.py
@@ -0,0 +1,44 @@
+"""
+Defines the base layer interface to be used by all layers.
+"""
+
+import layers
+
+import re
+
+class SaytexLayer:
+
+
+    @classmethod
+    def get_layer(cls, layer_id):
+        """
+        Creates a new layer of the layer_id subclass.
+        :param layer_id: str, representing the layer to create.
+        :return: a SaytexLayer object, representing a particular layer
+        """
+        return layers.layer_id_to_class[layer_id]()
+    
+
+    def execute_layer(self, input_string):
+        """
+        To be overridden in a layer subclass.
+        :param input_string: str, the input
+        :return: str, the output of the layer
+        """
+        return NotImplementedError
+
+
+
+    def replace_words(self, word_tuples, input_string):
+        """
+        Replaces words in input_string using the word_tuples.
+        :param word_tuples: a list of tuples of two strings, where the first one
+        is the word to be replaced and the second one is the word to replace it with
+        :input_string: str
+        :return: str, with words replaced
+        """
+        in_progress_string = input_string
+        for word_tuple in word_tuples:
+            s = r'\b' + word_tuple[0] + r'\b'
+            in_progress_string = re.sub(s, word_tuple[1], in_progress_string)
+        return in_progress_string
