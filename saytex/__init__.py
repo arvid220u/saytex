5b009d072b44cee753f12f759d870a16a37173c9
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2019-04-29 18:52:49 -0400

    add docstring to saytex package

--- saytex/__init__.py
+++ saytex/__init__.py
@@ -1,5 +1,13 @@
+"""
+The saytex package mainly consists of the Saytex class, used for
+converting from natural language to LaTeX, as well as the SaytexSyntax
+class, used for converting from the intermediary SayTeX Syntax into
+LaTeX.
+"""
+
 # import the core functionality
-from .compiler import Saytex
+from .compiler import Saytex, UnrecognizableSaytexInput
 
 # also import the saytex syntax
-from .saytexsyntax import SaytexSyntax
+from .saytexsyntax import SaytexSyntax, SaytexSyntaxError
+
