4581672249caf2437e4157d1299f9757ec627b60
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2019-04-28 21:24:14 -0400

    first versions of saytexplus

---
+++ saytexplus/__init__.py
@@ -0,0 +1,2 @@
+# import the core functionality
+from compiler import Saytex
---
+++ saytexplus/config.py
@@ -0,0 +1,8 @@
+# add config variables
+
+
+"""
+The used_layers is a set containing the layers to be used by SayTeX in
+converting natural language into SayTeX Syntax. The number of 
+"""
+used_layers = {}
