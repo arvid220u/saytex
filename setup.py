d861dcc2dbf5772cbfb0e1dfc84a21b3d319d3e5
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2019-05-05 23:04:28 -0400

    update docs location

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://saytex.xyz",
     project_urls=OrderedDict((
-        ('Documentation', 'http://saytex.xyz/docs'),
+        ('Documentation', 'https://saytex.readthedocs.io'),
         ('Code', 'https://github.com/arvid220u/saytex')
     )),
     packages=setuptools.find_packages(),
